676eff8a98027bd3416ee535e678f4be3b8ef5cd
Devin Smith <devin.gc.smith@live.com>
2020-10-01 20:19:12 +0100

    add missing source code

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -16,24 +8,24 @@ import Login from './components/login';
 const Stack = createStackNavigator();
 
 const App: () => React$Node = () => {
-  return (
-      <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen
-                name="Stratiphy"
-                component={WebSite}
-                options={{
-                    headerShown: false
-                }}
-            />
-            <Stack.Screen
-                name="Login"
-                component={Login}
-            />
-        </Stack.Navigator>
-      </NavigationContainer>
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Stratiphy"
+                    component={WebSite}
+                    options={{
+                        headerShown: false
+                    }}
+                />
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
 
-  );
+    );
 };
 
 
