d17e6ed6b2267fd7f424cdbba946e15ef9db409b
Devin Smith <devin.gc.smith@live.com>
2020-10-20 17:33:45 +0100

    add app center back in

--- ios/StratiphyApp/AppDelegate.m
+++ ios/StratiphyApp/AppDelegate.m
@@ -35,6 +35,10 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"StratiphyApp"
