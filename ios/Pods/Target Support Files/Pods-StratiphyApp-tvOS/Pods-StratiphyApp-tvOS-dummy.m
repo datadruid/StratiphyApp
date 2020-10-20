d13f1344b33b38dd09614f9383bf932db04ff705
Devin Smith <devin.gc.smith@live.com>
2020-10-20 13:13:26 +0100

    adding pods

---
+++ ios/Pods/Target Support Files/Pods-StratiphyApp-tvOS/Pods-StratiphyApp-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StratiphyApp_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_StratiphyApp_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-StratiphyApp-tvOSTests/Pods-StratiphyApp-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StratiphyApp_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_StratiphyApp_tvOSTests
+@end
